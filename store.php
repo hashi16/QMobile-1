ba97e1772b0203ddeebbf08ae419edfd29927d87
Dasith Deelaka <37569547+DasithDeelaka@users.noreply.github.com>
2020-03-11 14:54:15 +0530

    Remove paginations

--- store.php
+++ store.php
@@ -139,7 +139,7 @@
                         </div>
                 </div>
 
-            <div class="col-md-9">
+                <div class="col-md-9">
                     <?php include('include_store\header.php'); ?>
                     <!--Filter price range-->
                     <div class="row">
@@ -168,21 +168,6 @@
                             getProduct();
                         ?>
                     </div><br>
-                    
-                    <!--Paginations-->
-                    <div class="row">
-                        <div class="col-md-12">
-                            <nav aria-label="Page navigation example">
-                                <ul class="pagination justify-content-center">
-                                    <li class="page-item disabled"><a class="page-link" href="#" tabindex="-1">Previous</a></li>
-                                    <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                    <li class="page-item"><a class="page-link" href="#" tabindex="+1">Next</a></li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
